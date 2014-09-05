be123fe281d26cfdfce949036a65e28b36c14b98
Jonathan Hartley <tartley@tartley.com>
2014-09-05 22:40:03 +0100

    bump version to 1.0.1 to release Simeon's recent changes

--- colortuple/__init__.py
+++ colortuple/__init__.py
@@ -2,7 +2,7 @@
 A named tuple for storing RGBA colors.
 '''
 __name__ = 'colortuple'
-__version__ = '1.0.0rc2'
+__version__ = '1.0.1'
 
 # __doc__, __name__ &__version__ are seen by pydoc, read by setup.py,
 # and generate output for --help.
